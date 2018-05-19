d2ee80a1564711758a5085003a086bd696a5e690
aliasfly <zhangzexiangks@126.com>
2018-05-19 22:18:03 +0800

    P25

--- main.cc
+++ main.cc
@@ -1,15 +1,10 @@
 #include<iostream>
 using namespace std;
-inline int max(int a,int b,int c)
-{
-    if(b>a) a=b;
-    if(c>a) a=c;
-    return a;
-}
+float a=13.5;
 int main(void)
 {
-    int i=7,j=10,k=25,m;
-    m=max(i,j,k);
-    cout<<"max="<<m<<endl;
+    int a=5;
+    cout<<"a="<<a<<endl;
+    cout<<"a="<<::a<<endl;
     return 0;
 }
