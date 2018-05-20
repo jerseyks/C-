45bbcee2f0db59e951bf8194da8aa76d71710c44
aliasfly <zhangzexiangks@126.com>
2018-05-20 10:06:09 +0800

    P30

--- main.cc
+++ main.cc
@@ -1,32 +1,19 @@
 #include<iostream>
-#include<string>
+#include<string.h>
 using namespace std;
-string name[10];
+struct Student{
+    char name[100];
+    int num;
+    char sex;
+};
 int main(void)
 {
-    string st1,st2,st3,temp;
-    cout<<"please input three strings:";
-    cin>>st1>>st2>>st3;
-    if(st2>st3) 
-    {
-        temp=st2;
-        st2=st3;
-        st3=temp;
-    }
-    if(st1<=st2)
-    {
-        cout<<st1<<" "<<st2<<" "<<st3<<endl;
-    }
-    else if(st1<=st3)
-        {
-            cout<<st2<<" "<<st1<<" "<<st3<<endl;
-        }
-        else
-        {
-            cout<<st2<<" "<<st3<<" "<<st1<<endl;
-        }
-
-    cout<<"the size of string="<<sizeof(string)<<endl;
-    cout<<"the size of name="<<sizeof(name)<<endl;
+    Student *p;
+    p=new Student;
+    strcpy(p->name,"wang yun");
+    p->num=10123;
+    p->sex='M';
+    cout<<p->name<<" "<<p->num<<" "<<p->sex<<endl;
+    delete p;
     return 0;
 }
