8c697818c8bdd800700a5b1065c0c4d4b31ccd1b
aliasfly <zhangzexiangks@126.com>
2018-05-20 19:05:17 +0800

    P57-58

--- main.cc
+++ main.cc
@@ -8,21 +8,21 @@ class Time{
 };
 int main(void)
 {
-    void set_time(Time &t);
+    void set_time(Time &t,int hour=0,int minute=0,int sec=0);
     void show_time(Time &t);
     Time t1;
-    set_time(t1);
+    set_time(t1,12,23,34);
     show_time(t1);
     Time t2;
     set_time(t2);
     show_time(t2);
     return 0;
 }
-void set_time(Time &t)
+void set_time(Time &t,int hour,int minute,int sec)
 {
-    cin>>t.hour;
-    cin>>t.minute;
-    cin>>t.sec;
+    t.hour=hour;
+    t.minute=minute;
+    t.sec=sec;
 }
 void show_time(Time &t)
 {
