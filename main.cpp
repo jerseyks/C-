d7ffa5bf658b44303b9276fdc7ea176e92834cab
jersey91 <jersey91@126.com>
2019-08-31 21:19:13 +0800

    1-1

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 #include<class.h>
 
+#include<iostream>
+using namespace std;
+
 int main(void)
 {
-    Box box1;
-    cout<<"The volume of box1 is "<<box1.volume()<<endl;
-    Box box2(15);
-    cout<<"The volume of box2 is "<<box2.volume()<<endl;
-    Box box3(15,30);
-    cout<<"The volume of box3 is "<<box3.volume()<<endl;
-    Box box4(15,30,20);
-    cout<<"The volume of box4 is "<<box4.volume()<<endl;
-    
+    cout<<"This is a C++ program.\n";
+
     return 0;
 }
