9505dd40eedaee1a9f5bc4e3fffa4b982ccbe556
jersey91 <jersey91@126.com>
2019-08-31 22:09:07 +0800

    1-5

--- main.cpp
+++ main.cpp
@@ -3,31 +3,15 @@
 #include<iostream>
 using namespace std;
 
-class Student
-{
-    private:
-        int num;
-        float score;
-    public:
-        void setdata(void)
-        {
-            cin>>num;
-            cin>>score;
-        };
-        void display(void)
-        {
-            cout<<"num="<<num<<endl;
-            cout<<"score="<<score<<endl;
-        };
-};
-
-Student stud1,stud2;
 int main(void)
 {
-    stud1.setdata();
-    stud2.setdata();
-    stud1.display();
-    stud2.display();
+    cout<<"please enter your name and age:"<<endl;
+    char name[10];
+    int age;
+    cin>>name;
+    cin>>age;
+    cout<<"your name is "<<name<<endl;
+    cout<<"ypur age is "<<age<<endl;
 
     return 0;
 }
