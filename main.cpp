570ea2da1581251199293f5d485d1e6514a6fac7
jersey91 <jersey91@126.com>
2019-09-01 10:26:39 +0800

    1-10

--- main.cpp
+++ main.cpp
@@ -3,15 +3,20 @@
 #include<iostream>
 using namespace std;
 
+void swap(int a,int b)
+{
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<"a="<<a<<" "<<"b="<<b<<endl;
+}
 
 int main(void)
 {
-    int a=10;
-    int &b=a;
-    a=a*a;
-    cout<<a<<" "<<b<<endl;;
-    b=b/5;
-    cout<<a<<" "<<b<<endl;
+    int i=3,j=5;
+    swap(i,j);
+    cout<<"i="<<i<<" "<<"j="<<j<<endl;
 
 
     return 0;
