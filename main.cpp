e0a719cdb50a1022f0606a729086ae1d1ee13848
jersey91 <jersey91@126.com>
2019-09-01 22:08:06 +0800

    2-6

--- main.cpp
+++ main.cpp
@@ -2,53 +2,40 @@
 
 #include<iostream>
 using namespace std;
-
-class Array_may
+class Time
 {
     public:
-        void set_value(void);
-        void max_value(void);
-        void show_value(void);
+        void set_time(void);
+        void show_time(void);
     private:
-        int array[10];
-        int max;
+        int hour;
+        int minute;
+        int sec;
 };
-
-
-void Array_may::set_value(void)
-{
-    int i;
-    for(i=0;i<10;i++)
-    {
-        cin>>array[i];
-    }
-}
-
-void Array_may::max_value(void)
-{
-    int i;
-    max=array[0];
-    for(i=0;i<10;i++)
-    {
-        if(max<array[i])
-            max=array[i];
-    }
-}
-
-void Array_may::show_value(void)
-{
-    cout<<"max="<<max<<endl;
-}
 
 int main(void)
 {
-    Array_may arrmax;
-
-    arrmax.set_value();
-    arrmax.max_value();
-    arrmax.show_value();
-
+    Time t;
+    t.set_time();
+    t.show_time();
+    
     return 0;
 }
+
+
+void Time::set_time(void)
+{
+    cin>>hour;
+    cin>>minute;
+    cin>>sec;
+}
+void Time::show_time(void)
+{
+    cout<<hour<<":"<<minute<<":"<<sec<<endl;
+}
+
+
+
+
 
 
