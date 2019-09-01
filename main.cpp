094ce9febdaede7d178ae1e0c1807e985857a7bc
jersey91 <jersey91@126.com>
2019-09-01 11:40:34 +0800

    1-16

--- main.cpp
+++ main.cpp
@@ -1,36 +1,26 @@
 #include<class.h>
 
 #include<iostream>
-#include<string>
+#include<string.h>
 using namespace std;
+struct Student
+{
+    char name[10];
+    int num;
+    char sex;
+};
 
 
 int main(void)
 {
-    string string1,string2,string3,temp;        //由小到大输出
-    cout<<"please input three string:"<<endl;
-    cin>>string1>>string2>>string3;
-    if(string2>string3)     //使串2<=串3     //2小3大
-    {
-        temp=string2;
-        string2=string3;
-        string3=temp;
-    }
-    if(string1<=string2)    //串1<串2，则串1<串2<串3
-    {
-        cout<<string1<<" "<<string2<<" "<<string3<<endl;
-    }else   //串2<串1，串2<串3
-        {
-            if(string1<=string3)    //串2<串1<串3
-            {
-                cout<<string2<<" "<<string1<<" "<<string3<<endl;
-            }else   //串2<串3<串1
-                {
-                    cout<<string2<<" "<<string3<<" "<<string1<<endl;
-                }
-        }
-    
-    
+    Student *p;
+    p=new Student;
+    strcpy(p->name,"Wang Yun");         //string.h
+    p->num=10123;
+    p->sex='M';
+
+    cout<<p->name<<" "<<p->num<<" "<<p->sex<<endl;
+    delete p;
 
     return 0;
 }
