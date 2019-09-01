3d31daac255cebe40d41aceca5ea0ebd608706d5
jersey91 <jersey91@126.com>
2019-09-01 10:29:41 +0800

    1-10-1

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 #include<iostream>
 using namespace std;
 
-void swap(int a,int b)
+void swap(int *p1,int *p2)  //把两个地址中的内容进行交换
 {
     int temp;
-    temp=a;
-    a=b;
-    b=temp;
-    cout<<"a="<<a<<" "<<"b="<<b<<endl;
+    temp=*p1;
+    *p1=*p2;
+    *p2=temp;
+    cout<<"*p1="<<*p1<<" "<<"*p2="<<*p2<<endl;
 }
 
 int main(void)
 {
     int i=3,j=5;
-    swap(i,j);
+    swap(&i,&j);        
     cout<<"i="<<i<<" "<<"j="<<j<<endl;
 
 
