3a226d16cc7a70443be526c0426fabe31ad4483f
jersey91 <jersey91@126.com>
2019-09-01 10:55:11 +0800

    1-14

--- main.cpp
+++ main.cpp
@@ -3,19 +3,13 @@
 #include<iostream>
 using namespace std;
 
-inline int max(int a,int b,int c)
-{
-    if(b>a) a=b;
-    if(c>a) a=c;
-    return a;
-}
+float a=13.5;
 
 int main(void)
 {
-    int i=7,j=10,k=25,m;
-    m=max(i,j,k);
-    cout<<"max="<<m<<endl;
-
+    int a=5;
+    cout<<a<<endl;
+    cout<<::a<<endl;
 
     return 0;
 }
