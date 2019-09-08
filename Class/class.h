7e3ab23c082b90c4155f92a0f1a5433000c831a5
jersey91 <jersey91@126.com>
2019-09-08 22:26:19 +0800

    6-1-1

--- Class/class.h
+++ Class/class.h
@@ -40,5 +40,46 @@ ostream &operator<<(ostream &output,const Point &p)
     return output;
 }
 
+class Circle:public Point
+{
+    public:
+        Circle(float x=0,float y=0,float r=0);
+        void setRadius(float);
+        float getRadius(void) const;
+        float area(void) const;
+        friend ostream &operator<<(ostream &,const Circle &);
+    private:
+        float radius;
+};
+
+Circle::Circle(float a,float b,float r):Point(a,b),radius(r)
+{
+
+}
+
+void Circle::setRadius(float r)
+{
+    radius=r;
+}
+
+float Circle::getRadius(void) const
+{
+    return radius;
+}
+
+
+float Circle::area(void) const
+{
+    return 3.14159*radius*radius;
+}
+
+ostream &operator<<(ostream &output,const Circle &c)
+{
+    output<<"Center=["<<c.x<<","<<c.y<<"],r="<<c.radius<<",area="<<c.area()<<endl;
+
+    return output;
+}
+
+
 
 #endif/*__CLASS_H*/
--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@
 
 int main(void)
 {               
-    Point p(3.5,6.4);
-    cout<<"x="<<p.getX()<<",y="<<p.getY()<<endl;
-    p.setPoint(8.5,6.8);
-    cout<<"p(new):"<<p<<endl;
+    Circle c(3.5,6.4,5.2);
+    cout<<"original circle: \nx="<<c.getX()<<",y="<<c.getY()<<",r="<<c.getRadius()<<",area="<<c.area()<<endl;
+    c.setRadius(7.5);
+    c.setPoint(5,5);
+    cout<<"new circle: \n"<<c;
+    Point &pRef=c;
+    cout<<"pRef:"<<pRef;
 
     return 0;
 }
